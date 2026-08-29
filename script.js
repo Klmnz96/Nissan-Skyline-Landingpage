fc8648be49c9621b27434eea1dbd0e24638dca2f
Fabian Klemenz <klemenz.f@outlook.com>
2026-08-29 13:32:52 +0200

    implement menu open toggle logic

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function init() {
+  setupSpecs();
+  setupMenu();
+}
+
+function setupSpecs() {
   const specsGrid = document.getElementById("specs-grid");
   specsGrid.innerHTML = renderSpecs(specs);
+}
+
+function setupMenu() {
+  const menuToggle = document.getElementById("menu-toggle");
+  menuToggle.addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  const menuOverlay = document.getElementById("menu-overlay");
+  menuOverlay.classList.toggle("is-open");
 }
